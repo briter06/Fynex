f294369ed988a08a294c827ceae6f2fcc39363e6
Briter Gonzalez <btg.developers@gmail.com>
2020-11-04 16:35:23 -0500

    first_template

--- Fynex/settings.py
+++ Fynex/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'fynex_app.apps.FynexAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -47,8 +49,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'Fynex.urls'
 
 TEMPLATES = [
@@ -118,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "fynex_app/static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
---
+++ fynex_app/static/js/login-modals.js
@@ -0,0 +1,71 @@
+var login_modal =
+  login_modal ||
+  (function ($) {
+    'use strict';
+
+    // Creating modal dialog's DOM
+    var $dialog = $(
+      '<div class="modal fade" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-hidden="true" style="padding-top:15%; overflow-y:visible;">' +
+      '<div class="modal-dialog modal-m">' +
+      '<div class="modal-content">' +
+      '<div class="modal-header"><h3 style="margin:0;"></h3><button style="background-color: Transparent;border: none;" onclick="login_modal.hide();">X</button></div>' +
+      '<div class="modal-body" align="center">' +
+      '<form method="POST">' +
+      $('#login_script').attr('token') +
+      '<input type="hidden" name="login" value="login"/>' +
+      '<label for="user_name">Username</label>' +
+      '<input required type="text" id="user_name" name="user_name">' +
+      '<label for="password">Password</label>' +
+      '<input required type="password" id="password" name="password">' +
+      '<div class="row">' +
+      '<a href="' + $('#login_script').attr('forgot_url') + '">Forgot your password</a>' +
+      '</div><div class="row"><div class="column-md-12 mx-auto">' +
+      '<input style="margin-top:20px" type="submit" value="Login"></input>' +
+      '</div></div>' +
+      '</form>' +
+      '</div>' +
+      '</div></div></div>'
+    );
+
+    return {
+      /**
+       * Opens our dialog
+       * @param message Custom message
+       * @param options Custom options:
+       * 				  options.dialogSize - bootstrap postfix for dialog size, e.g. "sm", "m";
+       * 				  options.progressType - bootstrap postfix for progress bar type, e.g. "success", "warning".
+       */
+      show: function (message, options) {
+        // Assigning defaults
+        if (typeof options === 'undefined') {
+          options = {};
+        }
+        if (typeof message === 'undefined') {
+          message = 'Login';
+        }
+        var settings = $.extend(
+          {
+            dialogSize: 'm',
+            progressType: '',
+            onHide: null, // This callback runs after the dialog was hidden
+          },
+          options
+        );
+        $dialog.find('h3').text(message);
+        // Adding callbacks
+        if (typeof settings.onHide === 'function') {
+          $dialog.off('hidden.bs.modal').on('hidden.bs.modal', function (e) {
+            settings.onHide.call($dialog);
+          });
+        }
+        // Opening dialog
+        $dialog.modal();
+      },
+      /**
+       * Closes dialog
+       */
+      hide: function () {
+        $dialog.modal('hide');
+      },
+    };
+  })(jQuery);
