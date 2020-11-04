ac983a9196782a2c6e2f123111e52f431ec18506
Briter Gonzalez <btg.developers@gmail.com>
2020-11-04 16:53:54 -0500

    Debug_Host

--- Fynex/settings.py
+++ Fynex/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '-u)i!gyx_#uob097y=q+*ygw0_wa2%-0t26im$qq90bd=ffzxv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fynexapp.herokuapp.com']
 
 
 # Application definition
