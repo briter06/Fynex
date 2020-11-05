2bdfe976ed731fd734f4f69c6845b82672d296e7
Briter Gonzalez <btg.developers@gmail.com>
2020-11-05 10:25:37 -0500

    Login_First

--- fynex_app/views.py
+++ fynex_app/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import messages
+from django.contrib.auth import authenticate
 from .recommender.HybridRecommender import HybridRecommender
 
 # Create your views here.
 
+def login(request, template_name):
+    username = request.POST['user_name']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        return testRecommender(request)
+    else:
+        messages.error(
+            request, 'El usuario o la contraseña son incorrectos')
+    return HttpResponseRedirect(reverse(template_name))
 
 def index(request):
-    testRecommender()
-    return render(request, 'fynex_app/index.html')
+    if request.method == 'POST':
+        if 'login' in request.POST:
+            return login(request, 'Fynex-index')
+    else:
+        return render(request, 'fynex_app/index.html')
 
-def testRecommender():
+def testRecommender(request):
     heartRate = 90
     glucose = 180
     height = 1.65
@@ -20,4 +35,4 @@ def testRecommender():
     hr = HybridRecommender()
 
     result = hr.predict(heartRate,glucose,height,weight,age)
-    print(result['recommendations'])
+    return render(request,'fynex_app/res_temp.html',{"result":result['recommendations'].to_html()})
