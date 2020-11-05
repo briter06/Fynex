1b28f119d2e5db0b4d751bbad1f3491dde08a094
Briter Gonzalez <btg.developers@gmail.com>
2020-11-05 11:28:57 -0500

    Login_temp

--- fynex_app/urls.py
+++ fynex_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="Fynex-index"),
+    path('administrator', views.administrator_index, name="Administrator-index"),
 ]
--- fynex_app/views.py
+++ fynex_app/views.py
@@ -2,37 +2,57 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import messages
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login,logout
 from .recommender.HybridRecommender import HybridRecommender
 
 # Create your views here.
 
-def login(request, template_name):
+def login_user(request, template_name):
     username = request.POST['user_name']
     password = request.POST['password']
     user = authenticate(username=username, password=password)
-    if user is not None:
-        return testRecommender(request)
-    else:
+    if user is None:
         messages.error(
             request, 'El usuario o la contraseña son incorrectos')
+    else:
+        login(request, user)
     return HttpResponseRedirect(reverse(template_name))
+
+def verify_auth(request,group_name):
+    if not request.user.groups.filter(name='administrator').exists():
+        return False
+    return True
 
 def index(request):
     if request.method == 'POST':
         if 'login' in request.POST:
-            return login(request, 'Fynex-index')
+            return login_user(request, 'Fynex-index')
     else:
-        return render(request, 'fynex_app/index.html')
+        if request.user.is_authenticated:
+            if request.user.groups.filter(name='administrator').exists():
+                return HttpResponseRedirect(reverse('Administrator-index'))
+            else:
+                return logout_user(request)
+        else:
+            return render(request, 'fynex_app/index.html')
 
-def testRecommender(request):
-    heartRate = 90
-    glucose = 180
-    height = 1.65
-    weight = 65
-    age = 55
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('Fynex-index'))
 
-    hr = HybridRecommender()
+def administrator_index(request):
+    if not verify_auth(request,'administrator'):
+        return HttpResponseRedirect(reverse('Fynex-index'))
+    if request.method=='POST':
+        return logout_user(request)
+    else:
+        heartRate = 90
+        glucose = 180
+        height = 1.65
+        weight = 65
+        age = 55
 
-    result = hr.predict(heartRate,glucose,height,weight,age)
-    return render(request,'fynex_app/res_temp.html',{"result":result['recommendations'].to_html()})
+        hr = HybridRecommender()
+
+        result = hr.predict(heartRate,glucose,height,weight,age)
+        return render(request,'fynex_app/administrator/res_temp.html',{"result":result['recommendations'].to_html()})
